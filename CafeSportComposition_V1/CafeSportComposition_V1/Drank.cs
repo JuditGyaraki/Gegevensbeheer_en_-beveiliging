4c4441a9ac410408bd349363970dcfc471bea958
JuditGyaraki <gyaraki.judit@gmail.com>
2025-09-01 21:03:36 +0200

    Cafe Sport verbeterde versie met composition

---
+++ CafeSportComposition_V1/CafeSportComposition_V1/Drank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeSportComposition_V1
+{
+    public class Drank
+    {
+        public string Naam;
+        public float Prijs;
+
+        public Drank(string naam, float prijs)
+        {
+            Naam = naam;
+            Prijs = prijs;
+        }
+    }
+}
